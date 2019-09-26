f0133385835ba35ce482a051e4477cfa93b909d9
Paul Geurts <paul.geurts@nl.abnamro.com>
2019-09-26 16:53:47 +0200

    Added circle graph examples

--- CircleGraphExample/ViewController.swift
+++ CircleGraphExample/ViewController.swift
@@ -10,13 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    @IBOutlet weak var circleGraphContainer: CircleGraph!
+    
+    let singleArcGraph = CircleGraph(frame: CGRect(x: 0, y: 0, width: 80, height: 80), strokeWidth: 5, passiveColor: UIColor.gray, activeColor: UIColor.yellow, reservedColor: UIColor.red)
+    let doubleArcGraph = CircleGraph(frame: CGRect(x: 0, y: 0, width: 120, height: 120), strokeWidth: 15, passiveColor: UIColor.red, activeColor: UIColor.blue, reservedColor: UIColor.green)
+    let threeArcGraph = CircleGraph(frame: CGRect(x: 0, y: 0, width: 200, height: 200), strokeWidth: 30, passiveColor: UIColor.gray, activeColor: UIColor.blue, reservedColor: UIColor.green)
+    
+//    var circleGraph: CircleGraph!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+//        circleGraph = CircleGraph(frame: circleGraphContainer.bounds, strokeWidth: 8, passiveColor: UIColor.lightGray, activeColor: UIColor.orange, reservedColor: UIColor.white)
+        
+//        circleGraphContainer.addSubview(circleGraph)
+        
+//        circleGraph = CircleGraph(frame: circleGraphContainer.bounds, strokeWidth: 8, passiveColor: UIColor.gray, activeColor: UIColor.orange)
+        
+        self.view.addSubview(singleArcGraph)
+        self.view.addSubview(doubleArcGraph)
+        self.view.addSubview(threeArcGraph)
+        singleArcGraph.translatesAutoresizingMaskIntoConstraints = false
+        doubleArcGraph.translatesAutoresizingMaskIntoConstraints = false
+        threeArcGraph.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            singleArcGraph.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            singleArcGraph.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 25),
+            singleArcGraph.widthAnchor.constraint(equalToConstant: 80),
+            singleArcGraph.heightAnchor.constraint(equalToConstant: 80)
+            ])
+        
+        NSLayoutConstraint.activate([
+            doubleArcGraph.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            doubleArcGraph.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            doubleArcGraph.widthAnchor.constraint(equalToConstant: 120),
+            doubleArcGraph.heightAnchor.constraint(equalToConstant: 120)
+            ])
+        
+        NSLayoutConstraint.activate([
+            threeArcGraph.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            threeArcGraph.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -25),
+            threeArcGraph.widthAnchor.constraint(equalToConstant: 200),
+            threeArcGraph.heightAnchor.constraint(equalToConstant: 200)
+            ])
+        
+        singleArcGraph.drawTrackPath()
+        doubleArcGraph.drawTwoLayerArc(from: 0, to: 0.5, animationDuration: 2.0)
+        threeArcGraph.drawThreeLayerArc(from: 0.75, to: 0.5, toto: 0.25, animationDuration: 3.0)
+        
         
     }
 
 
 }
 
+//@IBOutlet weak var graphContainer: UIView!
+//var graphView: CircleGraph!
+//    graphView = CircleGraph(frame: graphContainer.bounds, strokeWidth: 40, passiveColor: UIContext.darkGrayColor, activeColor: UIContext.abnGreenColor)
+//    graphContainer.addSubview(graphView)
