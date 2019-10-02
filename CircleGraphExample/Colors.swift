42264a5665cbe7009a8d7fc046b2703d00ce738e
Paul Geurts <paul.geurts@nl.abnamro.com>
2019-10-02 17:00:21 +0200

    Moved colors to global file

--- CircleGraphExample/Colors.swift
+++ CircleGraphExample/Colors.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+import UIKit
+
+struct Color {
+    static let cityLights: UIColor = UIColor(displayP3Red: 223/255, green: 230/255, blue: 233/255, alpha: 1)
+    static let fadedPoster: UIColor = UIColor(displayP3Red: 129/255, green: 236/255, blue: 236/255, alpha: 1)
+    static let eggBlue: UIColor = UIColor(displayP3Red: 0/255, green: 206/255, blue: 201/255, alpha: 1)
+    static let lightGreenishBlue: UIColor = UIColor(displayP3Red: 85/255, green: 239/255, blue: 196/255, alpha: 1)
+    static let pinkGlamour: UIColor = UIColor(displayP3Red: 255/255, green: 118/255, blue: 117/255, alpha: 1)
+}
--- CircleGraphExample/ViewController.swift
+++ CircleGraphExample/ViewController.swift
@@ -10,14 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    struct  Color {
-        static let cityLights = UIColor(displayP3Red: 223/255, green: 230/255, blue: 233/255, alpha: 1)
-        static let fadedPoster: UIColor = UIColor(displayP3Red: 129/255, green: 236/255, blue: 236/255, alpha: 1)
-        static let eggBlue: UIColor = UIColor(displayP3Red: 0/255, green: 206/255, blue: 201/255, alpha: 1)
-        static let lightGreenishBlue: UIColor = UIColor(displayP3Red: 85/255, green: 239/255, blue: 196/255, alpha: 1)
-        static let pinkGlamour: UIColor = UIColor(displayP3Red: 255/255, green: 118/255, blue: 117/255, alpha: 1)
-    }
-    
     //TO DO:
     // Update readme with screenshots
 
@@ -65,8 +57,8 @@ class ViewController: UIViewController {
     
     private func drawGraphs() {
         trackPathGraph.drawTrackPath()
-        singleArcAnimatedGraph.drawTwoLayerArc(from: 0, to: 0.5, animationDuration: 1, animationType: .easeOut)
-        doubleArcAnimatedGraph.drawThreeLayerArc(from: 0.8, to: 0.6, inBetween: 0.4, animationDuration: 2, animationType: .easeInEaseOut)
+        singleArcAnimatedGraph.drawTwoLayerArc(from: 0, to: 0.5, animationDuration: 0.75, animationType: .easeOut)
+        doubleArcAnimatedGraph.drawThreeLayerArc(from: 0.8, to: 0.6, inBetween: 0.4, animationDuration: 1.5, animationType: .easeInEaseOut)
     }
     
     private func highlightBorder() {
